e867ad878adbc0045ecd3835e4d4c41b4427ebd9
Deimond_2702.l <solmix02.27@gmail.com>
2025-12-09 17:05:53 -0500

    seguimos con bucle while

--- while.php
+++ while.php
@@ -3,5 +3,33 @@
 /**
  * ESTRUCTURAS REPETITIVAS (Ciclos | Bucles | Loops)
  * 
- * Las estructuras repe
+ * Las estructuras repetitivas son aquellas que reiteran una o un grupo de instrucciones "n" veces y dependen de una variable de control ciclo llamada contador. Es decir, ejecutan una o varias instrucciones un número de veces definido.
+ * 
+ * Los cuatro bucles en PHP son el bucle WHILE, el bucle FOR, el bucle DO-WHILE y el bucle FOREACH.
  */
+
+/**
+ * CICLO WHILE(Mientras que)
+ * 
+ * La instrucción while(mientras) ejecuta una porción de programa mientras se cumpla una cierta condición. Mientras la condición sea verdadera, se ejecutan las instrucciones contenidas en el while. Cuando deja de cumplirse la condición, se sale del ciclo y se continúa ejecutando el resto del programa.
+ * 
+ * Estructuras del while
+ * 
+ *  while(Condición){
+ *  Código a ejecutar 
+ * }
+ * 
+ * 
+ *  while(Condición):
+ *  Código a ejecutar 
+ *  endwhile;
+ */
+
+
+
+    $c=1;
+    $num=7;
+    while($c<=12){
+        echo $num*$c."<br>";
+        $c++;
+    }
